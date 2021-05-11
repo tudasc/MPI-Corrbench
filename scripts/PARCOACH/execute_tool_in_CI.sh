79039b8eb0fa040233a9ca8c830b56e9b7e30086
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2021-05-11 16:04:03 +0200

    use opt-9 istead of opt

--- scripts/PARCOACH/execute_tool_in_CI.sh
+++ scripts/PARCOACH/execute_tool_in_CI.sh
@@ -7,7 +7,7 @@ TIMEOUT_CMD="/usr/bin/timeout -k 120 120"
 
 clang -DEXCLUDE_PARCOACH_UNSUPPORTED_FUNCTIONS -c -g $(mpicc --showme:compile) -emit-llvm testcase.c -o testcase.bc
 
-$TIMEOUT_CMD opt -load $PARCOACH_ROOT/build/src/aSSA/aSSA.* -parcoach -check-mpi < testcase.bc > /dev/null 2> output.txt
+$TIMEOUT_CMD opt-9 -load $PARCOACH_ROOT/build/src/aSSA/aSSA.* -parcoach -check-mpi < testcase.bc > /dev/null 2> output.txt
 
 echo "Dbg: Parcoach output:"
 cat output.txt
