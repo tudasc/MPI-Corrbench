594b2877cbd8d7b8a77da971739b1008ccbeb095
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2021-05-11 16:49:13 +0200

    removed debug output

--- scripts/PARCOACH/execute_tool_in_CI.sh
+++ scripts/PARCOACH/execute_tool_in_CI.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-which opt
-which opt-9
-
 TIMEOUT_CMD="/usr/bin/timeout -k 120 120"
 
 clang -DEXCLUDE_PARCOACH_UNSUPPORTED_FUNCTIONS -c -g $(mpicc --showme:compile) -emit-llvm testcase.c -o testcase.bc
 
 $TIMEOUT_CMD opt-9 -load $PARCOACH_ROOT/build/src/aSSA/aSSA.* -parcoach -check-mpi < testcase.bc > /dev/null 2> output.txt
-
-echo "Dbg: Parcoach output:"
-cat output.txt
-echo ""
