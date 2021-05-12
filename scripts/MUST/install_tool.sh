11ca88d93c35f605510dc1b15724ddfdf5ea2c36
Tim Jammer <tim.jammer@tu-darmstadt.de>
2021-05-12 15:44:55 +0200

    use newly released version of MUST

--- scripts/MUST/install_tool.sh
+++ scripts/MUST/install_tool.sh
@@ -3,10 +3,10 @@ mkdir MUST_install
 
 INSTALL_DIR=$(pwd)/MUST_install
 
-wget https://hpc.rwth-aachen.de/must/files/MUST-v1.6.tar.gz
-tar -xzf MUST-v1.6.tar.gz
+wget https://hpc.rwth-aachen.de/must/files/MUST-v1.7.tar.gz
+tar -xzf MUST-v1.7.tar.gz
 
-cd MUST-v1.6
+cd MUST-v1.7
 mkdir build && cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR ..
