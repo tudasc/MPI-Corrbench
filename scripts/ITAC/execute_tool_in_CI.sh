80b35048729d4a0678f8d8b81fe90d8a6e15fd76
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2021-06-08 09:41:28 +0200

    Feat/ci itac (#4)
    Updates CI and link to evaluation-dashboard
    
    - Include the link to the dashboard 
    - include status badge for dashboard but NOT for this repository

---
+++ scripts/ITAC/execute_tool_in_CI.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TIMEOUT_CMD="/usr/bin/timeout -k 120 120"
+
+# in $(pwd) will be the file testcase.c , that should be evaluated
+
+mpiicc -cc=gcc -g testcase.c -lm
+
+mpirun -n 2 -check_mpi -genv VT_CHECK_TRACING on ./a.out 1> output.txt 2>&1
+
