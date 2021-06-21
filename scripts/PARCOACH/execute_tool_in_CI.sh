8c1fc9117c14c846eb96e76ad029692ecf4b7f39
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2021-06-21 18:50:09 +0200

    changed order of compiler flags

--- scripts/PARCOACH/execute_tool_in_CI.sh
+++ scripts/PARCOACH/execute_tool_in_CI.sh
@@ -2,7 +2,9 @@
 
 TIMEOUT_CMD="/usr/bin/timeout -k 120 120"
 
-clang -DEXCLUDE_PARCOACH_UNSUPPORTED_FUNCTIONS -c -g $(mpicc --showme:compile) -emit-llvm testcase.c -o testcase.bc
+mpicc --showme:compile
+
+clang -DEXCLUDE_PARCOACH_UNSUPPORTED_FUNCTIONS $(mpicc --showme:compile) -c -g -emit-llvm testcase.c -o testcase.bc
 
 $TIMEOUT_CMD opt-9 -load $PARCOACH_ROOT/build/src/aSSA/aSSA.* -parcoach -check-mpi < testcase.bc > /dev/null 2> output.txt
 
