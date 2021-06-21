f653d636e58de3d27a47b8c5ba41c7cc73d0f8d9
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2021-06-21 18:59:52 +0200

    removed debug code so that the log is not bloated

--- scripts/PARCOACH/execute_tool_in_CI.sh
+++ scripts/PARCOACH/execute_tool_in_CI.sh
@@ -5,5 +5,3 @@ TIMEOUT_CMD="/usr/bin/timeout -k 120 120"
 clang-9 -DEXCLUDE_PARCOACH_UNSUPPORTED_FUNCTIONS $(mpicc --showme:compile) -c -g -emit-llvm testcase.c -o testcase.bc
 
 $TIMEOUT_CMD opt-9 -load $PARCOACH_ROOT/build/src/aSSA/aSSA.* -parcoach -check-mpi < testcase.bc > /dev/null 2> output.txt
-
-cat output.txt
