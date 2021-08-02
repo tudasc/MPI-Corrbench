0ccb5e72f659f79ebdb3e86b158077216fd549ba
Tim Jammer <tim.jammer@tu-darmstadt.de>
2021-08-02 20:29:00 +0200

    adds some errors about wrong threading levels

---
+++ micro-benches/0-level/openmp/threading/missing_init_thread.c
@@ -0,0 +1,30 @@
+#include <mpi.h>
+#include <omp.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/*
+ * A Threaded Program need to use MPI_Init Thread, even if one only requests the MPI_THREAD_FUNNELED level
+ */
+int main(int argc, char *argv[]) {
+  int myRank;
+  int buffer_out[10], buffer_in[10];
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+
+#pragma omp parallel for
+  for (int i = 0; i < 10; i++) {
+    buffer_out[i] = i * 10;
+  }
+
+  if (myRank == 0) {
+    MPI_Send(buffer_out, 10, MPI_INT, 1, 123, MPI_COMM_WORLD);
+  } else if (myRank == 1) {
+    MPI_Recv(buffer_in, 10, MPI_INT, 0, 123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
