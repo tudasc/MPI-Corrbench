253b8fde9809d1b3b6bf413691b190fcf75dcf85
Alexander Hück <hueck.alexander@gmail.com>
2021-08-12 13:51:57 +0200

    reduce data race

---
+++ micro-benches/0-level/openmp/data_race/data_race_reduce.c
@@ -0,0 +1,63 @@
+#include "nondeterminism.h"
+
+#include <mpi.h>
+#include <omp.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define BUFFER_LENGTH_INT 100
+#define BUFFER_LENGTH_BYTE (BUFFER_LENGTH_INT * sizeof(int))
+
+#define NUM_THREADS 2
+
+bool has_error(const int *buffer, int expected) {
+  for (int i = 0; i < BUFFER_LENGTH_INT; ++i) {
+    if (buffer[i] != expected) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int main(int argc, char *argv[]) {
+  int provided;
+  const int requested = MPI_THREAD_SERIALIZED;
+
+  MPI_Init_thread(&argc, &argv, requested, &provided);
+  if (provided < requested) {
+    has_error_manifested(false);
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  }
+
+  int size;
+  int rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  int reduced_data[BUFFER_LENGTH_INT];
+  int local_data[BUFFER_LENGTH_INT];
+  fill_message_buffer(reduced_data, BUFFER_LENGTH_BYTE, 0);
+  fill_message_buffer(local_data, BUFFER_LENGTH_BYTE, 6);
+
+  const int value = 5;
+
+#pragma omp parallel
+  {
+#pragma omp for nowait
+    for (int i = 0; i < BUFFER_LENGTH_INT; i++) {
+      local_data[i] = local_data[i] + value;
+    }
+//#pragma omp barrier // fixes the data race
+#pragma omp single
+    MPI_Reduce(local_data, reduced_data, BUFFER_LENGTH_INT, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  }
+
+  if (rank == 0) {
+    const bool error = has_error(reduced_data, size * value);
+    has_error_manifested(error);
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
