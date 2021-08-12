95bc8cc997bb1d16ab78e03ec4986e4e20a44bb6
Alexander Hück <hueck.alexander@gmail.com>
2021-08-12 22:11:41 +0200

    task dep race with reduce

---
+++ micro-benches/0-level/openmp/data_race/data_race_task.c
@@ -0,0 +1,55 @@
+#include "nondeterminism.h"
+
+#include <mpi.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// DRB136
+int main(int argc, char *argv[]) {
+  int provided;
+  const int requested = MPI_THREAD_MULTIPLE;
+
+  MPI_Init_thread(&argc, &argv, requested, &provided);
+  if (provided < requested) {
+    has_error_manifested(false);
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+  }
+
+  int size;
+  int rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  int a = 1;
+  int b = 2;
+  int c = 3;
+  int d = 0;
+
+#pragma omp parallel
+#pragma omp single
+  {
+#pragma omp task depend(in : a)  // depend(inout : c)
+    {
+      int reduced_a = 0;
+      MPI_Reduce(&a, &reduced_a, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+      c += reduced_a;
+    }
+#pragma omp task depend(in : b)  // depend(inout : c)
+    {
+      int reduced_b = 0;
+      MPI_Reduce(&b, &reduced_b, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+      c += reduced_b;
+    }
+#pragma omp task depend(in : c) depend(out : d)
+    { MPI_Reduce(&c, &d, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD); }
+  }
+
+  if (rank == 0) {
+    const bool error = (d != 12);
+    has_error_manifested(error);
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
