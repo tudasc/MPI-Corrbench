819dbbcf4885a12beada436168c6d92bb55a2842
Tim Jammer <tim.jammer@tu-darmstadt.de>
2021-08-18 16:18:08 +0200

    adds some testcases with 2 collective operations that are forbidden by the standard

---
+++ micro-benches/0-level/openmp/collectives/two_collectives_2.c
@@ -0,0 +1,45 @@
+#include "nondeterminism.h"
+#include <mpi.h>
+#include <omp.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+ * the user had to make shure only one collective initialization is active at a time per process and communicator
+ * according to the standard (see p303)
+ */
+
+#define BUFFER_LENGTH_INT 100
+#define BUFFER_LENGTH_BYTE (BUFFER_LENGTH_INT * sizeof(int))
+int main(int argc, char *argv[]) {
+  int myRank, size;
+  int provided;
+  const int requested = MPI_THREAD_MULTIPLE;
+
+  MPI_Init_thread(&argc, &argv, requested, &provided);
+  if (provided < requested) {
+    has_error_manifested(false);
+    exit(EXIT_FAILURE);
+  }
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  const int other_rank = size - myRank - 1;
+
+  if (myRank == 0) {
+#pragma omp parallel num_threads(2)
+    { MPI_Barrier(MPI_COMM_WORLD); }  // end parallel
+  }
+
+  else {        // other MPI rank
+    usleep(5);  // "force" other Process to call the barrier two times at the same time
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);  // prevent deadlock
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
