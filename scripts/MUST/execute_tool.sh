e6066f5309c1f1824a94fd9b333acff958720aa9
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2021-09-16 15:18:16 +0200

    setup MUST for multi-threaded usage

--- scripts/MUST/execute_tool.sh
+++ scripts/MUST/execute_tool.sh
@@ -8,14 +8,14 @@ TIMEOUT_CMD="/usr/bin/timeout -k 120 120"
 # in $(pwd) ist eine datei testcase.c , die ausgewertet weeden soll
 
 # without tool
-/usr/bin/time --format "%e,%M" -o time_compile_baseline mpicc -g -fopenmp testcase.c -lm
+/usr/bin/time --format "%e,%M" -o time_compile_baseline $MPICC -g -fopenmp testcase.c -lm
 
 /usr/bin/time --format "%e,%M" -o time_run_baseline $TIMEOUT_CMD mpirun -n 2 ./a.out
 
 # with tool
-/usr/bin/time --format "%e,%M" -o time_compile mpicc -g -fopenmp testcase.c -lm
+/usr/bin/time --format "%e,%M" -o time_compile $MPICC -g -fopenmp testcase.c -lm
 
-/usr/bin/time --format "%e,%M" -o time_run $TIMEOUT_CMD mustrun -n 2 ./a.out
+/usr/bin/time --format "%e,%M" -o time_run $TIMEOUT_CMD mustrun --must:hybrid -n 2 ./a.out
 
 echo "$(tail -n 1 time_compile_baseline),$( tail -n 1 time_compile)" >> compile_overhead.csv
 echo "$(tail -n 1 time_run_baseline),$( tail -n 1 time_run)" >> run_overhead.csv
