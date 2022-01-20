a91ec0b9186faac6deeedaad09fbcccb5bee51f4
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-01-20 20:05:22 +0100

    fix double clause in opm pragma

--- micro-benches/0-level/openmp/ordering/two_collectives_6.c
+++ micro-benches/0-level/openmp/ordering/two_collectives_6.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
 
   const int other_rank = size - myRank - 1;
 
-#pragma omp parallel num_threads(2) num_threads(NUM_THREADS)
+#pragma omp parallel num_threads(NUM_THREADS)
   {
 #pragma omp task
     {
