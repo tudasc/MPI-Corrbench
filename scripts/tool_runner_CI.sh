9e46c3674f1885ea04a3e1e8a7b3ffc5be953666
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2022-12-01 15:36:51 +0100

    add merge for visualization

--- scripts/tool_runner_CI.sh
+++ scripts/tool_runner_CI.sh
@@ -70,6 +70,6 @@ done
 
 echo "executed Tool on all Testcases"
 
-python3 $SCRIPTS_DIR/Parser.py . $TOOL --outfile results_$TOOL.json
+python3 $SCRIPTS_DIR/Parser.py . $TOOL --outfile results_$CATEGORY_$TOOL.json
 
 echo "Gathered all results"
